9b89d3c1736ebf8fadebef93d7ad6266c8e793d4
lookjota <lookjota@gmail.com>
2024-05-10 10:30:14 -0300

    Primeiro commit

---
+++ src/main/java/jota/look/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,13 @@
+package jota.look.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    
+    private String message;
+    private String field;
+
+}
